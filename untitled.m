311a8bee981916e550bc5cdec2d9e10ef0f0f566
Micha <mlk04@wp.pl>
2019-04-29 15:23:09 +0200

    image and angular vel done

--- untitled.m
+++ untitled.m
@@ -1,20 +1,19 @@
-%ipaddress = '192.168.18.101';
-%rosinit(ipaddress) 
-
-rosinit('127.0.0.1')
+IP_OF_TURTLEBOT = '192.168.18.90'
+IP_OF_HOST_COMPUTER = '192.168.18.223'
+rosinit(IP_OF_TURTLEBOT,'NodeHost',IP_OF_HOST_COMPUTER);
 velocity = 0.1;     % meters per second
 
 if ismember('/bottom_kinect/rgb/image_raw', rostopic('list'))
     imsub = rossubscriber('/bottom_kinect/rgb/image_raw');
 end
 
-robot = rospublisher('/mux_vel_nav/cmd_vel') ;
+robot = rospublisher('/mux_vel_keyboard/cmd_vel') ;
 velmsg = rosmessage(robot);
 se = strel('line',11,90);
 
 while 1
     velmsg.Linear.X = velocity;
-    %send(robot,velmsg);
+    send(robot,velmsg);
     
     img = receive(imsub)
     img_erode = imerode(readImage(img),se);
